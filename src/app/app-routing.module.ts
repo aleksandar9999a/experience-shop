490e89eaa365a47ecb5d3a510d67499cbd4632cb
aleksandar9999a <aleksandar9999a@gmail.com>
2020-01-09 08:53:40 +0200

    Added router for sign component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import { HomeComponent } from './home/home.component';
 import { CatalogComponent } from './catalog/catalog.component';
 import { CreateAdvertisementComponent } from './create-advertisement/create-advertisement.component';
 import { AboutComponent } from './about/about.component';
+import { SignComponent } from './sign/sign.component';
 
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'catalog', component: CatalogComponent },
   { path: 'createAdvertisement', component: CreateAdvertisementComponent },
-  { path: 'about', component: AboutComponent }
+  { path: 'about', component: AboutComponent },
+  { path: 'sign', component: SignComponent }
 ];
 
 @NgModule({
