d99b6e9748c3e928873a388622ec778d645562f9
aleksandar9999a <aleksandar9999a@gmail.com>
2020-01-09 14:29:07 +0200

    Added logOut function

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { NotifierService } from "angular-notifier";
+import { Router } from '@angular/router';
+
+import fire from '../config/firebase.js';
 
 @Component({
   selector: 'app-navbar',
@@ -6,10 +10,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+  isHere: boolean;
+  constructor(private readonly notifier: NotifierService, private routerService: Router) { }
 
-  constructor() { }
+  logOut() {
+    fire.auth()
+      .signOut()
+      .then(res => {
+        this.notifier.notify('success', 'Successful!');
+        this.routerService.navigate(['/']);
+        location.reload();
+      })
+      .catch(err => this.notifier.notify('warning', err.message));
+  }
 
   ngOnInit() {
+    fire.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.isHere = true;
+      }
+      
+    })
   }
 
 }
