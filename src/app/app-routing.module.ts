8639966a8beb1847cadf956525dbf966cc9758d2
aleksandar9999a <aleksandar9999a@gmail.com>
2020-01-09 06:00:04 +0200

    Added routes for home, catalog, advertisement and about page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { CatalogComponent } from './catalog/catalog.component';
+import { CreateAdvertisementComponent } from './create-advertisement/create-advertisement.component';
+import { AboutComponent } from './about/about.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'catalog', component: CatalogComponent },
+  { path: 'createAdvertisement', component: CreateAdvertisementComponent },
+  { path: 'about', component: AboutComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
