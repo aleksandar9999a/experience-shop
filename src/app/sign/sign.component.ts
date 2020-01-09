361bbbce0988a646319864bc0e51fb8c2766dc9e
aleksandar9999a <aleksandar9999a@gmail.com>
2020-01-09 12:13:30 +0200

    Added FormGroup, Control, Firebase, Notifier and signIn Function

--- src/app/sign/sign.component.ts
+++ src/app/sign/sign.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { NotifierService } from "angular-notifier";
+
+import fire from '../config/firebase.js';
 
 @Component({
   selector: 'app-sign',
@@ -6,8 +10,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign.component.css']
 })
 export class SignComponent implements OnInit {
+  private readonly notifier: NotifierService;
 
-  constructor() { }
+  signInForm = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl('')
+  })
+  
+  constructor(notifierService: NotifierService) { this.notifier = notifierService; }
+
+  signIn(){
+    const {email, password} = this.signInForm.value;
+    fire.auth()
+      .signInWithEmailAndPassword(email, password)
+      .then(res => {
+        this.notifier.notify('success', 'Successful!');
+      })
+      .catch(err => this.notifier.notify('warning', err.message));
+  }
 
   ngOnInit() {
   }
