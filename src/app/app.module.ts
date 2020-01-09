85dff75a3b6329acf9874904acf1c0e5e14d93d7
aleksandar9999a <aleksandar9999a@gmail.com>
2020-01-09 05:55:06 +0200

    Added home, catalog and about components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { CatalogComponent } from './catalog/catalog.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    HomeComponent,
+    CatalogComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
