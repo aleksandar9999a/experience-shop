40141cabb86c40aec989295215df5ba4c268178a
aleksandar9999a <aleksandar9999a@gmail.com>
2020-01-09 16:26:17 +0200

    Get Item from Firebase

--- src/app/catalog/catalog.component.ts
+++ src/app/catalog/catalog.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
+import fire from '../config/firebase.js';
+
 @Component({
   selector: 'app-catalog',
   templateUrl: './catalog.component.html',
   styleUrls: ['./catalog.component.css']
 })
 export class CatalogComponent implements OnInit {
-
+  items;
   constructor() { }
 
   ngOnInit() {
+    fire.database().ref('/items').once('value').then(snapshot => {
+      this.items = Object.values(snapshot.val());
+      
+    })
   }
 
 }
