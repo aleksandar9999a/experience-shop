7de468f472cb20fc68c71fc9e24c5d69367397ab
aleksandar9999a <aleksandar9999a@gmail.com>
2020-01-10 15:56:38 +0200

    Added animation for Sign Form

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,16 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { NotifierService } from "angular-notifier";
 import { Router } from '@angular/router';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 import fire from '../config/firebase.js';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
+  animations: [
+    trigger('signState', [
+      state('hide', style({
+        opacity: 0
+      })),
+      state('show', style({
+        opacity: 1
+      })),
+      transition('hide <=> show', animate('0.5s'))
+    ])
+  ]
 })
 export class NavbarComponent implements OnInit {
   isHere: boolean;
+  signFormState = 'hide';
   constructor(private readonly notifier: NotifierService, private routerService: Router) { }
 
   logOut() {
@@ -22,6 +35,10 @@ export class NavbarComponent implements OnInit {
         location.reload();
       })
       .catch(err => this.notifier.notify('warning', err.message));
+  }
+
+  toggle() {
+    this.signFormState === 'show' ? this.signFormState = 'hide' : this.signFormState = 'show';
   }
 
   ngOnInit() {
