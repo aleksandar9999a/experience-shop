62ffecff9840e970929d72a99753f778874bb5ab
aleksandar9999a <aleksandar9999a@gmail.com>
2020-01-10 15:55:30 +0200

    Remove button for login

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,24 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
-import fire from '../config/firebase.js';
-
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  isHere: boolean;
 
   constructor(private routerService: Router) { }
 
   ngOnInit() {
-    fire.auth().onAuthStateChanged(user => {
-      if (user) {
-        this.isHere = true;
-      }
-    })
   }
 
 }
