7f675415991a0e8c3d832ec3e0ed481a0b362dbe
aleksandar9999a <aleksandar9999a@gmail.com>
2020-01-12 13:38:59 +0200

    Initial

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,41 @@
+import { Injectable, Output, EventEmitter } from '@angular/core';
+
+import fire from './../config/firebase';
+
+@Injectable()
+export class UserService {
+    @Output() isUserLogged: EventEmitter<boolean> = new EventEmitter();
+
+    setIsHere(res, userIsHere) {
+        this.isUserLogged.emit(userIsHere);
+        return res;
+    }
+
+    logIn(email, password) {
+        return fire.auth()
+            .signInWithEmailAndPassword(email, password)
+            .then(res => this.setIsHere(res, true));
+    }
+
+    createUser(email, password) {
+        return fire.auth()
+            .createUserWithEmailAndPassword(email, password)
+            .then(res => this.setIsHere(res, true));
+    }
+
+    logOut() {
+        return fire.auth().signOut().then(res => this.setIsHere(res, false));
+    }
+
+    checkIsHere() {
+        fire.auth().onAuthStateChanged(user => {
+            let isHere;
+            if (user) {
+                isHere = true;
+            } else {
+                isHere = false;
+            }
+            this.isUserLogged.emit(isHere);
+        });
+    }
+}
