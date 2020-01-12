038a2c82ca2ee16c3496e224aea8d6ee37208f8c
aleksandar9999a <aleksandar9999a@gmail.com>
2020-01-12 15:12:46 +0200

    Added animations

--- src/app/create-advertisement/create-advertisement.component.ts
+++ src/app/create-advertisement/create-advertisement.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { createAdvAnimations } from './create-advertisement.animations';
+import { CreateFormAnimations } from '../services/createFormAnimations.service';
 
 @Component({
   selector: 'app-create-advertisement',
   templateUrl: './create-advertisement.component.html',
-  styleUrls: ['./create-advertisement.component.css']
+  styleUrls: ['./create-advertisement.component.css'],
+  animations: createAdvAnimations
 })
 export class CreateAdvertisementComponent implements OnInit {
+  createFormState: string = 'close';
 
-  constructor() { }
+  constructor(private createFormAnimationsService: CreateFormAnimations) { }
 
   ngOnInit() {
+    this.createFormAnimationsService.change.subscribe(isOpen => isOpen ? this.createFormState = 'open' : this.createFormState = 'close');
   }
 
 }
