9ff2131982f71c57580439bb172690349a72306a
aleksandar9999a <aleksandar9999a@gmail.com>
2020-01-12 13:39:33 +0200

    Add UserService and remove firebase from here

--- src/app/sign/sign.component.ts
+++ src/app/sign/sign.component.ts
@@ -4,8 +4,8 @@ import { NotifierService } from "angular-notifier";
 import { SignFormService } from '../services/signForm.service';
 import { signAnimations } from './signAnimations';
 
-import fire from '../config/firebase.js';
 import { Router } from '@angular/router';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-sign',
@@ -27,13 +27,17 @@ export class SignComponent implements OnInit {
     confirmPassword: new FormControl('')
   })
 
-  constructor(private readonly notifier: NotifierService, private routerService: Router, private signFormService: SignFormService) { }
+  constructor(
+    private readonly notifier: NotifierService, 
+    private routerService: Router, 
+    private signFormService: SignFormService,
+    private userService: UserService
+    ) { }
 
   signIn() {
     const { email, password } = this.signInForm.value;
-    fire.auth()
-      .signInWithEmailAndPassword(email, password)
-      .then(res => {
+    this.userService.logIn(email, password)
+      .then(_ => {
         this.closeSignForm();
         this.notifier.notify('success', 'Successful!');
         this.routerService.navigate(['/catalog'])
@@ -44,14 +48,13 @@ export class SignComponent implements OnInit {
   signUp() {
     const { email, password, confirmPassword } = this.signUpForm.value;
     if (password === confirmPassword) {
-      fire.auth()
-        .createUserWithEmailAndPassword(email, password)
-        .then(res => {
-          this.closeSignForm();
-          this.notifier.notify('success', 'Successful!');
-          this.routerService.navigate(['/']);
-        })
-        .catch(err => this.notifier.notify('warning', err.message));
+      this.userService.createUser(email, password)
+      .then(_ => {
+        this.closeSignForm();
+        this.notifier.notify('success', 'Successful!');
+        this.routerService.navigate(['/catalog'])
+      })
+      .catch(err => this.notifier.notify('warning', err.message));
     } else {
       this.notifier.notify('warning', 'Confirm password is wrong!');
     }
@@ -62,13 +65,7 @@ export class SignComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.signFormService.change.subscribe(isOpen => {
-      if (isOpen) {
-        this.formState = 'open';
-      }else{
-        this.formState = 'close';
-      }
-    })
+    this.signFormService.change.subscribe(isOpen => isOpen ? this.formState = 'open' : this.formState = 'close')
   }
 
 }
