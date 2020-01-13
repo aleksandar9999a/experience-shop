b7bb8c05fce9e6aa114de14223af5e76ea48e21a
aleksandar9999a <aleksandar9999a@gmail.com>
2020-01-13 14:32:45 +0200

    Added Behavior and getAllItems

--- src/app/services/catalog.service.ts
+++ src/app/services/catalog.service.ts
@@ -1,7 +1,29 @@
 import { Injectable, Output } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable()
 export class CatalogService {
 
+    constructor(
+        private fireStore: AngularFirestore
+    ) { }
+
+    @Output() allItems = new BehaviorSubject([]);
+
+    getAllItems() {
+        this.fireStore
+            .collection('allItems')
+            .get()
+            .subscribe(shots => {
+                let newItems = []
+
+                shots.forEach(function (x) {
+                    newItems.push(x.data())
+                })
+
+                this.allItems.next(newItems);
+            });
+    }
 
 }
