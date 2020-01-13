436ea4ade2400c3601602b3eff0b7a5b67b36ab9
aleksandar9999a <aleksandar9999a@gmail.com>
2020-01-13 12:30:48 +0200

    Added firebase config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyB_f9BeIed-SFjz6PetHpXbTZNRFBc3caQ",
+    authDomain: "experience-shop.firebaseapp.com",
+    databaseURL: "https://experience-shop.firebaseio.com",
+    projectId: "experience-shop",
+    storageBucket: "experience-shop.appspot.com",
+    messagingSenderId: "832283039558",
+    appId: "1:832283039558:web:ce0f7eff6059f8b9783747",
+    measurementId: "G-LEHXWG3VY1"
+  }
 };
 
 /*
