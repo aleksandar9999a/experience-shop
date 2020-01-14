46f00f042398f9e5626184f1bf28b0ccb1f366cd
aleksandar9999a <aleksandar9999a@gmail.com>
2020-01-14 05:22:23 +0200

    Added animations

--- src/app/item-details/item-details.component.ts
+++ src/app/item-details/item-details.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { detailsFormAnimations } from './item-details.animations';
+import { DetailsFormAnimations } from '../services/item-details.animations.service';
 
 @Component({
   selector: 'app-item-details',
   templateUrl: './item-details.component.html',
-  styleUrls: ['./item-details.component.css']
+  styleUrls: ['./item-details.component.css'],
+  animations: detailsFormAnimations
 })
 export class ItemDetailsComponent implements OnInit {
+  detailsFormState: string = 'close';
 
-  constructor() { }
+  constructor(
+    private detailsAnimationsService: DetailsFormAnimations
+  ) { }
+
+  close() {
+    this.detailsAnimationsService.toggle();
+  }
 
   ngOnInit() {
+    this.detailsAnimationsService.change.subscribe(isOpen => isOpen ? this.detailsFormState = 'open' : this.detailsFormState = 'close')
   }
 
 }
