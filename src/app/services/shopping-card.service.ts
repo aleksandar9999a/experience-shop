3c4d3bdcc9bd36d561de4c6524d337e0ffdc31b2
aleksandar9999a <aleksandar9999a@gmail.com>
2020-01-16 14:11:50 +0200

    Added getShoppingItems and loadShoppingList

--- src/app/services/shopping-card.service.ts
+++ src/app/services/shopping-card.service.ts
@@ -1,15 +1,46 @@
 import { Injectable, Output, EventEmitter } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { UserService } from './user.service';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable()
 export class ShoppingCardService {
-
   isOpen = false;
 
+  constructor(
+    private fireStore: AngularFirestore,
+    private userService: UserService,
+  ){}
+
   @Output() changeFormState: EventEmitter<boolean> = new EventEmitter();
+  @Output() getShoppingItems = new BehaviorSubject([]);
+
+  async loadShoppingList(){
+    const uid = await this.userService.getCurrentUid();
+    
+    this.fireStore
+      .collection('userdata')
+      .doc(uid)
+      .collection('shoppingCard')
+      .get()
+      .subscribe(shots => {
+        let newItems = []
+
+        shots.forEach(function (x) {
+          newItems.push(x.data())
+        })
+
+        this.getShoppingItems.next(newItems);
+      });
+  }
 
   toggle(data) {
     this.isOpen = !this.isOpen;
     this.changeFormState.emit(this.isOpen);
+
+    if (this.isOpen) {
+      this.loadShoppingList();
+    }
   }
 
 }
