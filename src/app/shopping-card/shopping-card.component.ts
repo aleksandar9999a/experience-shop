4e0ac2b508d289dda445abf1a8976cc3ebbde4a7
aleksandar9999a <aleksandar9999a@gmail.com>
2020-01-16 06:03:27 +0200

    Added animations

--- src/app/shopping-card/shopping-card.component.ts
+++ src/app/shopping-card/shopping-card.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { ShoppingCardService } from '../services/shopping-card.service';
+import { formAnimations } from './shopping-card.animations';
 
 @Component({
   selector: 'app-shopping-card',
   templateUrl: './shopping-card.component.html',
-  styleUrls: ['./shopping-card.component.css']
+  styleUrls: ['./shopping-card.component.css'],
+  animations: formAnimations
 })
 export class ShoppingCardComponent implements OnInit {
+  formState: string = 'close';
 
-  constructor() { }
+  constructor(
+    private shoppingCardService: ShoppingCardService
+  ) { }
+
+  close() {
+    this.shoppingCardService.toggle({});
+  }
 
   ngOnInit() {
+    this.shoppingCardService.changeFormState.subscribe(isOpen => isOpen ? this.formState = 'open' : this.formState = 'close')
   }
 
 }
