3e848ed816a5987015c0ebd154336dbcb2cd15d6
aleksandar9999a <aleksandar9999a@gmail.com>
2020-01-16 13:16:24 +0200

    Remove changeDataState

--- src/app/services/shopping-card.service.ts
+++ src/app/services/shopping-card.service.ts
@@ -1,5 +1,4 @@
 import { Injectable, Output, EventEmitter } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
 
 @Injectable()
 export class ShoppingCardService {
@@ -7,12 +6,10 @@ export class ShoppingCardService {
   isOpen = false;
 
   @Output() changeFormState: EventEmitter<boolean> = new EventEmitter();
-  @Output() changeDataState: BehaviorSubject<Object> = new BehaviorSubject({});
 
   toggle(data) {
     this.isOpen = !this.isOpen;
     this.changeFormState.emit(this.isOpen);
-    this.changeDataState.next(data);
   }
 
 }
