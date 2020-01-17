2029bb43136cfb407d69f6fe4cb53c81a32a52d2
aleksandar9999a <aleksandar9999a@gmail.com>
2020-01-17 12:27:21 +0200

    Added createItemsElements

--- src/app/services/catalog.service.ts
+++ src/app/services/catalog.service.ts
@@ -1,28 +1,31 @@
 import { Injectable, Output } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { BehaviorSubject } from 'rxjs';
+import { Item } from '../interfaces/item.interface';
 
 @Injectable()
 export class CatalogService {
-
+    items: Array<Item>;
     constructor(
         private fireStore: AngularFirestore
     ) { }
 
-    @Output() allItems = new BehaviorSubject([]);
+    @Output() allItems = new BehaviorSubject(this.items);
 
+    createItemsElements(item){
+        let newItem = item.data();
+        newItem.id = item.id;
+        this.items.push(newItem);
+      }
+    
     getAllItems() {
         this.fireStore
             .collection('allItems')
             .get()
             .subscribe(shots => {
-                let newItems = []
-
-                shots.forEach(function (x) {
-                    newItems.push(x.data())
-                })
-
-                this.allItems.next(newItems);
+                this.items = [];
+                shots.forEach(this.createItemsElements.bind(this))
+                this.allItems.next(this.items);
             });
     }
 
