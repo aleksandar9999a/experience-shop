08ef2489ed0a5396a22a6e5e18b9e615b6221a23
aleksandar9999a <aleksandar9999a@gmail.com>
2020-01-17 16:55:24 +0200

    Added uploadImage, getUrl, edit

--- src/app/services/edit-form.service.ts
+++ src/app/services/edit-form.service.ts
@@ -1,10 +1,21 @@
 import { Injectable, Output, EventEmitter } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
+import { AngularFireStorage } from '@angular/fire/storage';
+import { NotifierService } from 'angular-notifier';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Injectable()
 export class EditFormService {
 
   isOpen = false;
+
+  constructor(
+    private readonly notifier: NotifierService,
+    private firebaseStorage: AngularFireStorage,
+    private firabaseAuth: AngularFireAuth,
+    private fireStore: AngularFirestore
+  ) { }
 
   @Output() change: EventEmitter<boolean> = new EventEmitter();
   @Output() changeData: BehaviorSubject<any> = new BehaviorSubject({})
@@ -13,6 +24,28 @@ export class EditFormService {
     this.isOpen = !this.isOpen;
     this.change.emit(this.isOpen);
     this.changeData.next(data || {});
+  }
+
+  async uploadImage(image) {
+    const storageRef = this.firebaseStorage.ref(`items/${image.name}`);
+    return await storageRef.put(image).then(this.getUrl).catch(err => this.notifier.notify('warning', err.message));
+  }
+
+  async getUrl(snapshot) {
+    return await snapshot.ref.getDownloadURL().catch(err => this.notifier.notify('warning', err.message));
+  }
+
+  async edit(id, name, desc, image, price, type) {
+    let url = image;
+
+    if (typeof image === 'object') {
+      url = await this.uploadImage(image);
+    }
+
+    const user = this.firabaseAuth.auth.currentUser;
+    const newItem = { creatorUid: user.uid, name, desc, image: url, price, type };
+
+    return await this.fireStore.collection('allItems').doc(id).update(newItem);
   }
 
 }
