b4102ba004b37203582fa1b730434feca33de049
aleksandar9999a <aleksandar9999a@gmail.com>
2020-01-17 15:02:09 +0200

    Added delete

--- src/app/services/item-details.service.ts
+++ src/app/services/item-details.service.ts
@@ -1,11 +1,18 @@
 import { Injectable, Output, EventEmitter } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { Item } from '../interfaces/item.interface';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { UserService } from './user.service';
 
 @Injectable()
 export class DetailsFormService {
 
   isOpen = false;
+
+  constructor(
+    private fireStore: AngularFirestore,
+    private userService: UserService
+  ) { }
 
   @Output() changeFormState: EventEmitter<boolean> = new EventEmitter();
   @Output() changeDataState: BehaviorSubject<any> = new BehaviorSubject({});
@@ -14,6 +21,15 @@ export class DetailsFormService {
     this.isOpen = !this.isOpen;
     this.changeFormState.emit(this.isOpen);
     this.changeDataState.next(data || {});
+  }
+
+  async getShoppingCardRef(uid){
+    return await this.fireStore.collection('userdata').doc(uid).collection('shoppingCard');
+  }
+
+  async delete(id){
+    const uid = await this.userService.getCurrentUid();
+    return this.fireStore.collection('allItems').doc(id).delete();
   }
 
 }
