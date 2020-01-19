717920760467bf202d09ed5b4339deeac1672074
aleksandar9999a <aleksandar9999a@gmail.com>
2020-01-19 13:18:40 +0200

    Added types and notifications

--- src/app/services/edit-form.service.ts
+++ src/app/services/edit-form.service.ts
@@ -4,38 +4,38 @@ import { AngularFireStorage } from '@angular/fire/storage';
 import { NotifierService } from 'angular-notifier';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { AngularFirestore } from '@angular/fire/firestore';
+import { Item } from '../interfaces/item.interface';
 
 @Injectable()
 export class EditFormService {
-
   isOpen = false;
 
   constructor(
     private readonly notifier: NotifierService,
     private firebaseStorage: AngularFireStorage,
     private firabaseAuth: AngularFireAuth,
-    private fireStore: AngularFirestore
+    private fireStore: AngularFirestore,
   ) { }
 
   @Output() change: EventEmitter<boolean> = new EventEmitter();
   @Output() changeData: BehaviorSubject<any> = new BehaviorSubject({})
 
-  toggle(data?) {
+  toggle(data?: Item) {
     this.isOpen = !this.isOpen;
     this.change.emit(this.isOpen);
     this.changeData.next(data || {});
   }
 
-  async uploadImage(image) {
+  private async uploadImage(image) {
     const storageRef = this.firebaseStorage.ref(`items/${image.name}`);
     return await storageRef.put(image).then(this.getUrl).catch(err => this.notifier.notify('warning', err.message));
   }
 
-  async getUrl(snapshot) {
+  private async getUrl(snapshot) {
     return await snapshot.ref.getDownloadURL().catch(err => this.notifier.notify('warning', err.message));
   }
 
-  async edit(id, name, desc, image, price, type) {
+  async edit(id: string, name: string, desc: string, image: any, price: number, type: string) {
     let url = image;
 
     if (typeof image === 'object') {
@@ -45,7 +45,11 @@ export class EditFormService {
     const user = this.firabaseAuth.auth.currentUser;
     const newItem = { creatorUid: user.uid, name, desc, image: url, price, type };
 
-    return await this.fireStore.collection('allItems').doc(id).update(newItem);
+    await this.fireStore.collection('allItems').doc(id).update(newItem)
+      .then(_ => {
+        this.notifier.notify('success', 'Successful edited!');
+      })
+      .catch(err => this.notifier.notify('warning', err.message));
   }
 
 }
