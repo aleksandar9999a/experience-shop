23fdd026634641afe22a6fdc717bc73de55d9030
aleksandar9999a <aleksandar9999a@gmail.com>
2020-01-19 13:41:13 +0200

    Move functions for announcements edit to new service

--- src/app/services/edit-form.service.ts
+++ src/app/services/edit-form.service.ts
@@ -10,12 +10,7 @@ import { Item } from '../interfaces/item.interface';
 export class EditFormService {
   isOpen = false;
 
-  constructor(
-    private readonly notifier: NotifierService,
-    private firebaseStorage: AngularFireStorage,
-    private firabaseAuth: AngularFireAuth,
-    private fireStore: AngularFirestore,
-  ) { }
+  constructor() { }
 
   @Output() change: EventEmitter<boolean> = new EventEmitter();
   @Output() changeData: BehaviorSubject<any> = new BehaviorSubject({})
@@ -25,31 +20,4 @@ export class EditFormService {
     this.change.emit(this.isOpen);
     this.changeData.next(data || {});
   }
-
-  private async uploadImage(image) {
-    const storageRef = this.firebaseStorage.ref(`items/${image.name}`);
-    return await storageRef.put(image).then(this.getUrl).catch(err => this.notifier.notify('warning', err.message));
-  }
-
-  private async getUrl(snapshot) {
-    return await snapshot.ref.getDownloadURL().catch(err => this.notifier.notify('warning', err.message));
-  }
-
-  async edit(id: string, name: string, desc: string, image: any, price: number, type: string) {
-    let url = image;
-
-    if (typeof image === 'object') {
-      url = await this.uploadImage(image);
-    }
-
-    const user = this.firabaseAuth.auth.currentUser;
-    const newItem = { creatorUid: user.uid, name, desc, image: url, price, type };
-
-    await this.fireStore.collection('allItems').doc(id).update(newItem)
-      .then(_ => {
-        this.notifier.notify('success', 'Successful edited!');
-      })
-      .catch(err => this.notifier.notify('warning', err.message));
-  }
-
 }
