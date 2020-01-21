d8f55608297edc6ba7266b637f736eb65ee4d967
aleksandar9999a <aleksandar9999a@gmail.com>
2020-01-21 14:18:54 +0200

    Subscribe for isUserLogged and set isHere

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgZone } from '@angular/core';
 import { UserService } from './services/user.service';
 
 @Component({
@@ -7,12 +7,21 @@ import { UserService } from './services/user.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-
+  isHere: boolean;
+  
   constructor(
-    private userService: UserService
+    private userService: UserService,
+    private zone: NgZone
   ) { }
+
+  setIsHere(currState: boolean){
+    this.zone.run(() => {
+      this.isHere = currState;
+    });
+  }
 
   ngOnInit() {
     this.userService.checkIsHere();
+    this.userService.isUserLogged.subscribe(this.setIsHere.bind(this));
   }
 }
