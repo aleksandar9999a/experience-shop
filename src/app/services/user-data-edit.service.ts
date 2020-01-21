a33f15a6e375e3669123d041dc4b9466a654009d
aleksandar9999a <aleksandar9999a@gmail.com>
2020-01-21 11:07:15 +0200

    Added updateUserData

--- src/app/services/user-data-edit.service.ts
+++ src/app/services/user-data-edit.service.ts
@@ -1,11 +1,24 @@
 import { Injectable, Output, EventEmitter } from '@angular/core';
 import { Profile } from '../interfaces/profile.interface';
 import { BehaviorSubject } from 'rxjs';
+import { AngularFireStorage } from '@angular/fire/storage';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { UserService } from './user.service';
+import { NotifierService } from 'angular-notifier';
 
 @Injectable()
 export class UserDataEditService {
 
   isOpen = false;
+
+  constructor(
+    private readonly notifier: NotifierService,
+    private firebaseStorage: AngularFireStorage,
+    private firabaseAuth: AngularFireAuth,
+    private fireStore: AngularFirestore,
+    private userService: UserService
+  ) { }
 
   @Output() change: EventEmitter<boolean> = new EventEmitter();
   @Output() changeInfo: BehaviorSubject<Object> = new BehaviorSubject({});
@@ -14,6 +27,38 @@ export class UserDataEditService {
     this.isOpen = !this.isOpen;
     this.change.emit(this.isOpen);
     this.changeInfo.next(data || {});
+  }
+
+  private async uploadImage(image: any) {
+    const d = new Date();
+    const n = d.getTime();
+    const storageRef = this.firebaseStorage.ref(`items/${n}`);
+    return await storageRef.put(image).then(this.getUrl).catch(err => this.notifier.notify('warning', err.message));
+  }
+
+  private async getUrl(snapshot: any) {
+    return await snapshot.ref.getDownloadURL().catch(err => this.notifier.notify('warning', err.message));
+  }
+
+  async updateUserData(username: string, summary: string, profileImg: any) {
+    const uid = this.userService.getCurrentUid();
+
+    if (typeof profileImg !== 'string') {
+      profileImg = await this.uploadImage(profileImg);
+    }
+    
+    const info = { username, summary, profileImg };
+
+    this.fireStore
+      .collection('userdata')
+      .doc(uid)
+      .collection('userdata')
+      .doc('info')
+      .update(info)
+      .then(_ => {
+        this.notifier.notify('success', 'You successful update your information!');
+      })
+      .catch(err => this.notifier.notify('warning', err.message));
   }
 
 }
