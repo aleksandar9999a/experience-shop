daa98bbb54e52cb91eb9cfb96f452403bc818221
aleksandar9999a <aleksandar9999a@gmail.com>
2020-01-22 15:48:06 +0200

    Remove all functions and etc...

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,8 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Item } from '../interfaces/item.interface';
-import { Profile } from '../interfaces/profile.interface';
-import { UserDataEditService } from '../services/user-data-edit.service';
-import { ProfileService } from '../services/profile.service';
 
 @Component({
   selector: 'app-profile',
@@ -10,56 +6,10 @@ import { ProfileService } from '../services/profile.service';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
-  myItems: Array<Item>;
-  info: Profile;
-  isLoadingUserdata: boolean;
-  isLoadingItems: boolean;
-  isUnknown: boolean;
-  isItems: boolean;
-  profileImg: string = './../../assets/images/unkItem.svg';
 
-  constructor(
-    private userDataEditService: UserDataEditService,
-    private profileService: ProfileService
-  ) { }
-
-
-  private setInfo(shot: any) {
-    if (shot) {
-      this.info = shot;
-      if (this.info.profileImg) {
-        this.profileImg = this.info.profileImg;
-      }
-
-      this.isUnknown = false;
-    } else {
-      this.isUnknown = true;
-    }
-
-    this.isLoadingUserdata = false;
-  }
-
-  private setItems(items: Array<Item>) {
-    this.myItems = items;
-    this.isLoadingItems = false;
-
-    if (this.myItems.length > 0) {
-      this.isItems = true;
-    }else{
-      this.isItems = false;
-    }
-  }
-
-  openEditForm() {
-    this.userDataEditService.toggle(this.info);
-  }
+  constructor() { }
 
   ngOnInit() {
-    this.profileService.loadUserdata();
-    this.isLoadingItems = true;
-    this.isLoadingUserdata = true;
-    this.profileService.getUserdata.subscribe(this.setInfo.bind(this))
-    this.profileService.getUserItems.subscribe(this.setItems.bind(this));
   }
 
 }
