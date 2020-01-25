ed5a9b7a67994ff8539851711263c8b9243a293f
aleksandar9999a <aleksandar9999a@gmail.com>
2020-01-25 08:53:25 +0200

    Move functions, styles, html to catalog-list

--- src/app/catalog/catalog.component.ts
+++ src/app/catalog/catalog.component.ts
@@ -1,8 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { CatalogService } from '../services/catalog.service';
-import { Item } from '../interfaces/item.interface';
-import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
-import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-catalog',
@@ -11,31 +7,9 @@ import { Observable } from 'rxjs';
 })
 export class CatalogComponent implements OnInit {
 
-  private itemsCollection: AngularFirestoreCollection<Item>;
-  items: Observable<Item[]>;
-  isItems = false;
-  isLoading = false;
-
-  constructor(
-    private catalogService: CatalogService,
-    private afs: AngularFirestore
-  ) { }
-
-  setItems(shots: Array<Item>) {
-    shots.length > 0 ? this.isItems = true : this.isItems = false;
-    this.isLoading = false;
-  }
-
-  setSearchFunction(fn?) {
-    this.isLoading = true;
-    this.itemsCollection = this.afs.collection<Item>('allItems', fn);
-    this.items = this.itemsCollection.valueChanges();
-    this.items.subscribe(this.setItems.bind(this));
-  }
+  constructor() { }
 
   ngOnInit() {
-    this.setSearchFunction();
-    this.catalogService.getItemsFunction.subscribe(this.setSearchFunction.bind(this));
   }
 
 }
