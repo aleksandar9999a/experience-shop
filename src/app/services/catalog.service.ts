8df5f8fcea1f537efa4bbb6503a2f5ddb287ffd5
aleksandar9999a <aleksandar9999a@gmail.com>
2020-01-25 17:53:42 +0200

    Remove one if state

--- src/app/services/catalog.service.ts
+++ src/app/services/catalog.service.ts
@@ -9,15 +9,12 @@ export class CatalogService {
         name = name.toLocaleLowerCase();
 
         const searchByName = (ref: any) => ref.where('name', '>=', name);
-        const searchByCategory = (ref: any) => ref.where('category', '==', category);
         const searchByNameAndCategoty = (ref: any) => ref.where('category', '==', category).where('name', '>=', name);
 
         if (category === 'all') {
             this.getItemsFunction.emit(searchByName);
-        } else if (name && category) {
+        } else {
             this.getItemsFunction.emit(searchByNameAndCategoty);
-        } else if (category) {
-            this.getItemsFunction.emit(searchByCategory);
         }
     }
 
