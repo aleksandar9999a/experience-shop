b39b1a9c7f24fdc1bb38fdfb9778f46040dda61c
aleksandar9999a <aleksandar9999a@gmail.com>
2020-01-25 11:26:32 +0200

    Initial

---
+++ src/app/catalog-pagination/catalog-pagination.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-catalog-pagination',
+  templateUrl: './catalog-pagination.component.html',
+  styleUrls: ['./catalog-pagination.component.css']
+})
+export class CatalogPaginationComponent implements OnInit {
+  constructor() { }
+
+  nextPage() {
+  }
+
+  backPage() {
+  }
+
+  ngOnInit() {
+  }
+
+}
