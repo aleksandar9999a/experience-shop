7a43b09d2bef8496affc1d2e96a6cc404202e329
aleksandar9999a <aleksandar9999a@gmail.com>
2020-01-28 13:01:51 +0200

    Added ProfileSetUpComponent, ProfileSetUpFormService

--- src/app/authentication/authentication.module.ts
+++ src/app/authentication/authentication.module.ts
@@ -5,16 +5,18 @@ import { SignInComponent } from './sign-in/sign-in.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { AuthenticationFormService } from './services/authentication-form.service';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { ProfileSetUpComponent } from './profile-set-up/profile-set-up.component';
+import { ProfileSetUpFormService } from './services/profile-set-up.service';
 
 
 @NgModule({
-  declarations: [AuthenticationFormComponent, SignInComponent, SignUpComponent],
-  providers: [AuthenticationFormService],
+  declarations: [AuthenticationFormComponent, SignInComponent, SignUpComponent, ProfileSetUpComponent],
+  providers: [AuthenticationFormService, ProfileSetUpFormService],
   imports: [
     CommonModule,
     FormsModule,
     ReactiveFormsModule
   ],
-  exports: [AuthenticationFormComponent]
+  exports: [AuthenticationFormComponent, ProfileSetUpComponent]
 })
 export class AuthenticationModule { }
