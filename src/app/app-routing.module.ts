1ef88b0aafc335467b72c03862e44ae7f3661023
aleksandar9999a <aleksandar9999a@gmail.com>
2020-01-29 09:51:12 +0200

    Route for users catalog

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 import { CatalogComponent } from './catalog/catalog/catalog.component';
 import { ProfileComponent } from './profile/profile/profile.component';
 import { HomeComponent } from './core/home/home.component';
+import { UserCatalogComponent } from './user-catalog/user-catalog/user-catalog.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'catalog', component: CatalogComponent },
+  { path: 'users', component: UserCatalogComponent },
   { path: 'profile', component: ProfileComponent }
 ];
 
