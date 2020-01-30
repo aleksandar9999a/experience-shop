26d0aa77dc76f718c20dbacbb5b9cedf206dabe6
aleksandar9999a <aleksandar9999a@gmail.com>
2020-01-30 05:23:30 +0200

    Import SharedModule

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,13 +4,15 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
 import { AppRoutingModule } from '../app-routing.module';
 import { HomeComponent } from './home/home.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
   declarations: [NavbarComponent, FooterComponent, HomeComponent],
   imports: [
     CommonModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule
   ],
   exports: [NavbarComponent, FooterComponent, HomeComponent]
 })
