d54857fd4760a94018ae528b0b4eda3f90d6f324
aleksandar9999a <aleksandar9999a@gmail.com>
2020-02-02 09:31:41 +0200

    Init

---
+++ src/app/user-catalog/user-items-list/user-items-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-items-list',
+  templateUrl: './user-items-list.component.html',
+  styleUrls: ['./user-items-list.component.css']
+})
+export class UserItemsListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
