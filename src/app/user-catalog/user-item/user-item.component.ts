30c386e26c74c10a6e24895e3f7b0115ff57e2dd
aleksandar9999a <aleksandar9999a@gmail.com>
2020-02-02 10:15:53 +0200

    Added userItemsListService and searchByUserUID

--- src/app/user-catalog/user-item/user-item.component.ts
+++ src/app/user-catalog/user-item/user-item.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { IProfile } from 'src/app/interfaces/profile.interface';
 import { UserDetailsService } from '../services/user-details.service';
+import { UserItemsListService } from '../services/user-items-list.service';
 
 @Component({
   selector: 'app-user-item',
@@ -11,11 +12,13 @@ export class UserItemComponent implements OnInit {
   @Input() profile: IProfile;
 
   constructor(
-    private userDetailsService: UserDetailsService
+    private userDetailsService: UserDetailsService,
+    private userItemsListService: UserItemsListService
   ) { }
 
-  openUserDetails(){
+  openUserDetails() {
     this.userDetailsService.toggle(this.profile);
+    this.userItemsListService.searchByUserUID(this.profile.id);
   }
 
   ngOnInit() {
