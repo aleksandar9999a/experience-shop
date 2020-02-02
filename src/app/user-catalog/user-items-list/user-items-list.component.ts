9130e1be56e7266d5ec52d56e0fc80f875fea120
aleksandar9999a <aleksandar9999a@gmail.com>
2020-02-02 09:50:51 +0200

    Added Input, items, userItemsListService

--- src/app/user-catalog/user-items-list/user-items-list.component.ts
+++ src/app/user-catalog/user-items-list/user-items-list.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { IProfile } from 'src/app/interfaces/profile.interface';
+import { UserItemsListService } from '../services/user-items-list.service';
 
 @Component({
   selector: 'app-user-items-list',
@@ -6,10 +8,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-items-list.component.css']
 })
 export class UserItemsListComponent implements OnInit {
-
-  constructor() { }
+  @Input() user: IProfile;
+  get items() { return this.userItemsListService.items; }
+  constructor(
+    private userItemsListService: UserItemsListService
+  ) { }
 
   ngOnInit() {
+    this.userItemsListService.searchByUserUID(this.user.id);
   }
 
 }
