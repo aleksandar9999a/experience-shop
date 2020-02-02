6fbf6a8374ab8c500016cd90c2cb994de3de1a48
aleksandar9999a <aleksandar9999a@gmail.com>
2020-02-02 17:25:22 +0200

    Added AuthGuard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,29 @@ import { CatalogComponent } from './catalog/catalog/catalog.component';
 import { ProfileComponent } from './profile/profile/profile.component';
 import { HomeComponent } from './core/home/home.component';
 import { UserCatalogComponent } from './user-catalog/user-catalog/user-catalog.component';
+import { AuthGuard } from './auth.guard';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'catalog', component: CatalogComponent },
-  { path: 'users', component: UserCatalogComponent },
-  { path: 'profile', component: ProfileComponent }
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'catalog',
+    component: CatalogComponent,
+  },
+  {
+    path: 'users',
+    component: UserCatalogComponent,
+    canActivate: [AuthGuard],
+    data: { isHere: true }
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent,
+    canActivate: [AuthGuard],
+    data: { isHere: true }
+  }
 ];
 
 @NgModule({
