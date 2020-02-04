a44972d8f790096283965d2adb0fdad8d0ef2162
aleksandar9999a <aleksandar9999a@gmail.com>
2020-02-04 07:17:16 +0200

    Added searchFn, clean code

--- src/app/profile/services/my-items.service.ts
+++ src/app/profile/services/my-items.service.ts
@@ -8,19 +8,21 @@ import { UserService } from 'src/app/services/user.service';
 export class MyItemsService {
     private itemsCollection: AngularFirestoreCollection<IItem>;
     items: Observable<IItem[]>;
+
     get uid() { return this.userService.uid; }
 
     constructor(
         private afs: AngularFirestore,
         private userService: UserService
-    ) {
-        this.itemsCollection = this.afs.collection<IItem>('allItems', (ref: any) => ref.where('creatorUid', '==', this.uid));
+    ) { }
+
+    refreshItems() {
+        this.itemsCollection = this.afs.collection<IItem>('allItems', this.searchFn.bind(this));
         this.items = this.itemsCollection.valueChanges();
     }
 
-    refreshItems() {
-        this.itemsCollection = this.afs.collection<IItem>('allItems', (ref: any) => ref.where('creatorUid', '==', this.uid));
-        this.items = this.itemsCollection.valueChanges();
+    searchFn(ref: any) {
+        return ref.where('creatorUid', '==', this.uid);
     }
 
 }
