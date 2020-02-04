f8a429d8bca974e1e9fa50bcc12e986a05f24fd9
aleksandar9999a <aleksandar9999a@gmail.com>
2020-02-04 07:14:24 +0200

    Added type of limit

--- src/app/catalog/services/catalog.service.ts
+++ src/app/catalog/services/catalog.service.ts
@@ -23,7 +23,7 @@ export class CatalogService {
         private readonly notifier: NotifierService
     ) { }
 
-    setPageLimit(limit) {
+    setPageLimit(limit: number) {
         this.pageLimit = limit;
         this.loadList();
     }
