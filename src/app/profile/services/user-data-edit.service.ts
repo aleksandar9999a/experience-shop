81b6bd4990b3c710142b0d260af3b060ba1e16ab
aleksandar9999a <aleksandar9999a@gmail.com>
2020-02-04 04:35:14 +0200

    Remove updateUserData, uploadImage, getUrl

--- src/app/profile/services/user-data-edit.service.ts
+++ src/app/profile/services/user-data-edit.service.ts
@@ -8,16 +8,8 @@ import { IProfile } from 'src/app/interfaces/profile.interface';
 @Injectable()
 export class UserDataEditService {
   isOpen = false;
-  uid: string;
 
-  constructor(
-    private readonly notifier: NotifierService,
-    private firebaseStorage: AngularFireStorage,
-    private fireStore: AngularFirestore,
-    private firabaseAuth: AngularFireAuth
-  ) {
-    this.firabaseAuth.auth.onAuthStateChanged(user => user ? this.uid = user.uid : this.uid = null);
-  }
+  constructor( ) { }
 
   @Output() changeFormState: EventEmitter<boolean> = new EventEmitter();
   @Output() changeInfo: EventEmitter<IProfile> = new EventEmitter();
@@ -28,36 +20,6 @@ export class UserDataEditService {
 
     if (data) {
       this.changeInfo.emit(data);
-    }
-  }
-
-  private async uploadImage(image: any) {
-    const d = new Date();
-    const n = d.getTime();
-    const storageRef = this.firebaseStorage.ref(`items/${n}`);
-    return await storageRef.put(image).then(this.getUrl).catch(err => this.notifier.notify('warning', err.message));
-  }
-
-  private async getUrl(snapshot: any) {
-    return await snapshot.ref.getDownloadURL().catch(err => this.notifier.notify('warning', err.message));
-  }
-
-  async updateUserData(username: string, summary: string, profileImg: any) {
-    if (this.uid) {
-      if (typeof profileImg !== 'string') {
-        profileImg = await this.uploadImage(profileImg);
-      }
-      const info = { id: this.uid, username, summary, profileImg };
-
-      await this.fireStore
-        .doc(`userdata/${this.uid}`)
-        .set(info)
-        .then(_ => {
-          this.notifier.notify('success', 'You successful update your information!');
-        })
-        .catch(err => this.notifier.notify('warning', err.message));
-    } else {
-      this.notifier.notify('warning', 'You must be registered to edit your data!');
     }
   }
 
