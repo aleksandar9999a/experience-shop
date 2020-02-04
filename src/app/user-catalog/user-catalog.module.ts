ca06157cf10fd793d31841c8dea99263ea11bcbd
aleksandar9999a <aleksandar9999a@gmail.com>
2020-02-04 08:41:48 +0200

    Remove UserItemComponent

--- src/app/user-catalog/user-catalog.module.ts
+++ src/app/user-catalog/user-catalog.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { UserCatalogComponent } from './user-catalog/user-catalog.component';
 import { UserSearchComponent } from './user-search/user-search.component';
 import { UserListComponent } from './user-list/user-list.component';
-import { UserItemComponent } from './user-item/user-item.component';
 import { UserDetailsComponent } from './user-details/user-details.component';
 import { UserListControllerComponent } from './user-list-controller/user-list-controller.component';
 import { UserListNavigationComponent } from './user-list-navigation/user-list-navigation.component';
@@ -21,7 +20,6 @@ import { UserDetailsListNavigationComponent } from './user-details-list-navigati
     UserCatalogComponent,
     UserSearchComponent,
     UserListComponent,
-    UserItemComponent,
     UserDetailsComponent,
     UserListControllerComponent,
     UserListNavigationComponent,
