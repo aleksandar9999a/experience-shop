446af26437173221200e707527b1ba76037ebfb1
aleksandar9999a <aleksandar9999a@gmail.com>
2020-02-04 06:36:33 +0200

    Init

---
+++ src/app/profile/services/user-info.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { UserService } from 'src/app/services/user.service';
+import { IProfile } from 'src/app/interfaces/profile.interface';
+
+@Injectable()
+export class UserInfoService {
+    profileDoc: AngularFirestoreDocument<IProfile>;
+    profile: Observable<IProfile>;
+
+    get uid() { return this.userService.uid; }
+
+    constructor(
+        private afs: AngularFirestore,
+        private userService: UserService
+    ) {
+        this.profileDoc = afs.doc<IProfile>(`userdata/${this.uid}`);
+        this.profile = this.profileDoc.valueChanges();
+    }
+
+}
