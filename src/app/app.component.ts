82276de961ba6e351083199116218273b6ec2a43
aleksandar9999a <aleksandar9999a@gmail.com>
2020-02-08 15:22:25 +0200

    Remove user service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { UserService } from './services/user.service';
 
 @Component({
   selector: 'app-root',
@@ -7,11 +6,8 @@ import { UserService } from './services/user.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  get isHere() { return this.userService.isHere; }
 
-  constructor(
-    private userService: UserService
-  ) { }
+  constructor() { }
 
   ngOnInit() { }
 }
