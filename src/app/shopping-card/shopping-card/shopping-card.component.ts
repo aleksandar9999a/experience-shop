86ba46ec725f8f93239beed3f5b47b0f729839eb
aleksandar9999a <aleksandar9999a@gmail.com>
2020-02-09 15:13:55 +0200

    Added shopping-card-list

--- src/app/shopping-card/shopping-card/shopping-card.component.ts
+++ src/app/shopping-card/shopping-card/shopping-card.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ShoppingCardService } from '../services/shopping-card.service';
 import { formAnimations } from './shopping-card.animations';
-import { IItem } from 'src/app/interfaces/item.interface';
 import { Router } from '@angular/router';
 
 @Component({
@@ -11,29 +9,16 @@ import { Router } from '@angular/router';
   animations: formAnimations
 })
 export class ShoppingCardComponent implements OnInit {
-  get items() { return this.shoppingCardService.items; }
-  get price() { return this.shoppingCardService.price; }
 
   constructor(
-    private shoppingCardService: ShoppingCardService,
     private router: Router
   ) { }
-
-  clearShoppingCard() {
-    this.shoppingCardService.deleteAllItems();
-  }
-
-  buyAllProducts(newItems: Array<IItem>) {
-    this.shoppingCardService.buyAllProducts(newItems);
-    this.close();
-  }
 
   close() {
     this.router.navigate([{ outlets: { formsOutlet: [] } }]);
   }
 
   ngOnInit() {
-    this.shoppingCardService.loadItems();
   }
 
 }
