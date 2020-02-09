609e49f8aaee0ee687b9b0ca04b7c99d86ee01f9
aleksandar9999a <aleksandar9999a@gmail.com>
2020-02-09 16:21:17 +0200

    Added recInfo:IRecipientInformation

--- src/app/interfaces/shipment.interface.ts
+++ src/app/interfaces/shipment.interface.ts
@@ -1,7 +1,10 @@
+import { IRecipientInformation } from './recipientInformation.interface';
+
 export interface IShipment {
     listOfItems: Array<string>;
     receiver: string;
     sender: string;
     shipmentId: string;
     status: string;
+    recInfo: IRecipientInformation;
 }
