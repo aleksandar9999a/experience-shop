c845e5fcb6bfc25b982551e0a9e8547f5fc77734
aleksandar9999a <aleksandar9999a@gmail.com>
2020-02-09 14:18:42 +0200

    Init

---
+++ src/app/shared/directives/autocomplete.directive.ts
@@ -0,0 +1,31 @@
+import { Directive, ElementRef, HostListener } from '@angular/core';
+import { MapsAPILoader } from '@agm/core';
+import { NgControl } from '@angular/forms';
+
+@Directive({
+    selector: '[appAutocomplete]'
+})
+export class AutocompleteDirective {
+    autocomplete: any;
+    options = { types: ['address'], componentRestrictions: { country: 'bg' } };
+
+    constructor(
+        private el: ElementRef,
+        private mapsAPILoader: MapsAPILoader,
+        private control: NgControl
+    ) {
+        mapsAPILoader.load().then(() => {
+            this.autocomplete = new google.maps.places.Autocomplete(this.el.nativeElement, this.options);
+        });
+    }
+
+    @HostListener('keypress') onKeyPress() {
+        this.autocomplete.addListener('place_changed', this.setNewPlace.bind(this));
+    }
+
+    private setNewPlace() {
+        const place = this.autocomplete.getPlace();
+        this.control.control.setValue(place.formatted_address);
+    }
+
+}
