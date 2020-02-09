49e15f6cd3de4627189addffd517ea9c19c49801
aleksandar9999a <aleksandar9999a@gmail.com>
2020-02-09 14:18:18 +0200

    Added agm config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,14 +5,18 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyB_f9BeIed-SFjz6PetHpXbTZNRFBc3caQ",
-    authDomain: "experience-shop.firebaseapp.com",
-    databaseURL: "https://experience-shop.firebaseio.com",
-    projectId: "experience-shop",
-    storageBucket: "experience-shop.appspot.com",
-    messagingSenderId: "832283039558",
-    appId: "1:832283039558:web:ce0f7eff6059f8b9783747",
-    measurementId: "G-LEHXWG3VY1"
+    apiKey: 'AIzaSyB_f9BeIed-SFjz6PetHpXbTZNRFBc3caQ',
+    authDomain: 'experience-shop.firebaseapp.com',
+    databaseURL: 'https://experience-shop.firebaseio.com',
+    projectId: 'experience-shop',
+    storageBucket: 'experience-shop.appspot.com',
+    messagingSenderId: '832283039558',
+    appId: '1:832283039558:web:ce0f7eff6059f8b9783747',
+    measurementId: 'G-LEHXWG3VY1'
+  },
+  agm: {
+    apiKey: 'AIzaSyAWSZSCMxTRonYQe3L94wvCEd-gJCbrVkk',
+    libraries: ['places']
   }
 };
 
