60b29aea2b2f3e9900c81d8ba7203d7f4703f699
aleksandar9999a <aleksandar9999a@gmail.com>
2020-02-09 15:13:34 +0200

    Added ShoppingCardListComponent, ShoppingCardDetailsComponent

--- src/app/shopping-card/shopping-card.module.ts
+++ src/app/shopping-card/shopping-card.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { ShoppingCardComponent } from './shopping-card/shopping-card.component';
 import { ShoppingCardService } from './services/shopping-card.service';
 import { SharedModule } from '../shared/shared.module';
+import { ShoppingCardListComponent } from './shopping-card-list/shopping-card-list.component';
+import { ShoppingCardDetailsComponent } from './shopping-card-details/shopping-card-details.component';
 
 
 @NgModule({
-  declarations: [ShoppingCardComponent],
+  declarations: [ShoppingCardComponent, ShoppingCardListComponent, ShoppingCardDetailsComponent],
   providers: [ShoppingCardService],
   imports: [
     CommonModule,
