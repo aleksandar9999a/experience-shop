42ee94fea781a895fa304fefbe357bb3d39272d3
aleksandar9999a <aleksandar9999a@gmail.com>
2020-02-11 05:59:09 +0200

    Remove AuthenticationFormService, ProfileSetUpFormService

--- src/app/authentication/authentication.module.ts
+++ src/app/authentication/authentication.module.ts
@@ -3,15 +3,12 @@ import { CommonModule } from '@angular/common';
 import { AuthenticationFormComponent } from './authentication-form/authentication-form.component';
 import { SignInComponent } from './sign-in/sign-in.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
-import { AuthenticationFormService } from './services/authentication-form.service';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { ProfileSetUpComponent } from './profile-set-up/profile-set-up.component';
-import { ProfileSetUpFormService } from './services/profile-set-up.service';
 
 
 @NgModule({
   declarations: [AuthenticationFormComponent, SignInComponent, SignUpComponent, ProfileSetUpComponent],
-  providers: [AuthenticationFormService, ProfileSetUpFormService],
   imports: [
     CommonModule,
     FormsModule,
