89f808923b17ca209d1de2b547b97761d4e9864d
aleksandar9999a <aleksandar9999a@gmail.com>
2020-02-12 13:06:37 +0200

    Remove

--- src/app/profile/items-card/my-items/my-items.module.ts
+++ src/app/profile/items-card/my-items/my-items.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MyItemsService } from './services/my-items.service';
 import { MyItemsComponent } from './my-items/my-items.component';
 import { MyItemsNavigatorComponent } from './my-items-navigator/my-items-navigator.component';
 import { SharedModule } from 'src/app/shared/shared.module';
@@ -8,7 +7,6 @@ import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
   declarations: [MyItemsNavigatorComponent, MyItemsComponent],
-  providers: [MyItemsService],
   imports: [
     CommonModule,
     SharedModule
