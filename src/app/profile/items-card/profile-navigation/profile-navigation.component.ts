ba873b657f6cb71328c6e5aa4fadc18928a69951
aleksandar9999a <aleksandar9999a@gmail.com>
2020-02-12 14:32:54 +0200

    Added ICollectionOptions, CollectionsService, ListService, changeItems

--- src/app/profile/items-card/profile-navigation/profile-navigation.component.ts
+++ src/app/profile/items-card/profile-navigation/profile-navigation.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ICollectionOptions } from 'src/app/interfaces/coll-options.interface';
+import { CollectionsService } from 'src/app/services/collections.service';
+import { ListService } from '../services/list.service';
 
 @Component({
   selector: 'app-profile-navigation',
@@ -6,8 +9,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile-navigation.component.css']
 })
 export class ProfileNavigationComponent implements OnInit {
+  options: ICollectionOptions;
 
-  constructor() { }
+  constructor(
+    private collService: CollectionsService,
+    private listService: ListService
+  ) { }
+
+  changeItems(type: string) {
+    const types = {
+      myItems: {  position: 'firstPage', pageLimit: 5, collection: 'myItems' },
+      shipments: { category: 'receiver', position: 'firstPage', pageLimit: 5, collection: 'orders' },
+      orders: { category: 'sender', position: 'firstPage', pageLimit: 5, collection: 'orders' },
+    };
+
+    if (types[type]) {
+      this.collService.setOptions(types[type]);
+      this.listService.changeListType(type);
+    }
+  }
 
   ngOnInit() {
   }
