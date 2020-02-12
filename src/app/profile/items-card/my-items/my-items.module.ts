f3331968d92989a871a780c46856676b52cc4701
aleksandar9999a <aleksandar9999a@gmail.com>
2020-02-12 13:25:47 +0200

    Remove MyItemsNavigatorComponent

--- src/app/profile/items-card/my-items/my-items.module.ts
+++ src/app/profile/items-card/my-items/my-items.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MyItemsComponent } from './my-items/my-items.component';
-import { MyItemsNavigatorComponent } from './my-items-navigator/my-items-navigator.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 
 
 @NgModule({
-  declarations: [MyItemsNavigatorComponent, MyItemsComponent],
+  declarations: [MyItemsComponent],
   imports: [
     CommonModule,
     SharedModule
