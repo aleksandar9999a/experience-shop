1002cc9c80ba9b86d578f805ad6fde4916a3b41c
aleksandar9999a <aleksandar9999a@gmail.com>
2020-02-12 15:04:05 +0200

    Remove CatalogItemsCounterComponent

--- src/app/catalog/catalog.module.ts
+++ src/app/catalog/catalog.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SearchComponent } from './search/search.component';
-import { CatalogItemsCounterComponent } from './catalog-items-counter/catalog-items-counter.component';
 import { CatalogComponent } from './catalog/catalog.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { SharedModule } from '../shared/shared.module';
@@ -9,7 +8,6 @@ import { SharedModule } from '../shared/shared.module';
 @NgModule({
   declarations: [
     SearchComponent,
-    CatalogItemsCounterComponent,
     CatalogComponent
   ],
   imports: [
