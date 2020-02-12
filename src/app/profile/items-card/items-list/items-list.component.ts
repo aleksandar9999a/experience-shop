7b604a4805e06f41a81b7d3468ff6c0c60bbcbe8
aleksandar9999a <aleksandar9999a@gmail.com>
2020-02-12 14:32:27 +0200

    Added CollectionsService, ListService

--- src/app/profile/items-card/items-list/items-list.component.ts
+++ src/app/profile/items-card/items-list/items-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CollectionsService } from 'src/app/services/collections.service';
+import { ListService } from '../services/list.service';
 
 @Component({
   selector: 'app-items-list',
@@ -6,10 +8,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./items-list.component.css']
 })
 export class ItemsListComponent implements OnInit {
+  get items() { return this.collService.items; }
+  listType = 'myItems';
 
-  constructor() { }
+  constructor(
+    private collService: CollectionsService,
+    private listService: ListService
+  ) { }
+
+  setListType(type: string) {
+    this.listType = type;
+  }
 
   ngOnInit() {
+    this.collService.setOptions({ position: 'firstPage', pageLimit: 5, collection: 'myItems' });
+    this.listService.listType.subscribe(this.setListType.bind(this));
   }
 
 }
