5ef8306f374967e240639094e5d67e7dd6c8d114
aleksandar9999a <aleksandar9999a@gmail.com>
2020-02-12 13:08:53 +0200

    Remove OrdersService

--- src/app/profile/items-card/orders/orders.module.ts
+++ src/app/profile/items-card/orders/orders.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { OrdersComponent } from './orders/orders.component';
-import { OrdersService } from './services/orders.service';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { NavigationComponent } from './navigation/navigation.component';
 
 
 @NgModule({
   declarations: [OrdersComponent, NavigationComponent],
-  providers: [OrdersService],
   imports: [
     CommonModule,
     SharedModule
