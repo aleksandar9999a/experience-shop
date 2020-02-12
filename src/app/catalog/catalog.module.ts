12f9b0219a7aef1358fe6bc59c374f0ce8a8238f
aleksandar9999a <aleksandar9999a@gmail.com>
2020-02-12 13:25:18 +0200

    Remove CatalogPaginationComponent

--- src/app/catalog/catalog.module.ts
+++ src/app/catalog/catalog.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SearchComponent } from './search/search.component';
-import { CatalogPaginationComponent } from './catalog-pagination/catalog-pagination.component';
 import { CatalogItemsCounterComponent } from './catalog-items-counter/catalog-items-counter.component';
 import { CatalogComponent } from './catalog/catalog.component';
 import { ReactiveFormsModule } from '@angular/forms';
@@ -10,7 +9,6 @@ import { SharedModule } from '../shared/shared.module';
 @NgModule({
   declarations: [
     SearchComponent,
-    CatalogPaginationComponent,
     CatalogItemsCounterComponent,
     CatalogComponent
   ],
