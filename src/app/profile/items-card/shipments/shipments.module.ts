bc8d33672222a933f50ba33cdb61f401c2079fe6
aleksandar9999a <aleksandar9999a@gmail.com>
2020-02-12 13:09:21 +0200

    Remove ShipmentsService

--- src/app/profile/items-card/shipments/shipments.module.ts
+++ src/app/profile/items-card/shipments/shipments.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ShipmentsComponent } from './shipments/shipments.component';
-import { ShipmentsService } from './services/shipments.service';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { NavigationComponent } from './navigation/navigation.component';
 
 
 @NgModule({
   declarations: [ShipmentsComponent, NavigationComponent],
-  providers: [ShipmentsService],
   imports: [
     CommonModule,
     SharedModule
