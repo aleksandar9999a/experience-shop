fe5229e76aed01b71d73fe374e9eeedb6b7c9dbb
aleksandar9999a <aleksandar9999a@gmail.com>
2020-02-12 13:26:07 +0200

    Remove NavigationComponent

--- src/app/profile/items-card/shipments/shipments.module.ts
+++ src/app/profile/items-card/shipments/shipments.module.ts
@@ -2,11 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ShipmentsComponent } from './shipments/shipments.component';
 import { SharedModule } from 'src/app/shared/shared.module';
-import { NavigationComponent } from './navigation/navigation.component';
 
 
 @NgModule({
-  declarations: [ShipmentsComponent, NavigationComponent],
+  declarations: [ShipmentsComponent],
   imports: [
     CommonModule,
     SharedModule
