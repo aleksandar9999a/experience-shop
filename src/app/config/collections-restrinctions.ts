2bd8ce7d0ee35f62c1c7fe6917229ba95684041f
aleksandar9999a <aleksandar9999a@gmail.com>
2020-02-16 10:07:26 +0200

    Added private to restriction

--- src/app/config/collections-restrinctions.ts
+++ src/app/config/collections-restrinctions.ts
@@ -1,5 +1,5 @@
 export class OptionsRestrinctions {
-    restriction = {
+    private restriction = {
         allItems: {
             category: ['', 'all', 'pc', 'laptops', 'tablets', 'phone', 'accForPC', 'accForPhone', 'audio', 'photo', 'nav', 'tv', 'other'],
             position: ['firstPage', 'endBefore', 'startAfter', 'currentPage'],
