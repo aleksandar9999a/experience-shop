c9d791b80889c0092fc5f2bf0f1cae95e2f4b0ed
aleksandar9999a <aleksandar9999a@gmail.com>
2020-05-23 14:38:43 +0300

    Remove keys

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,17 +5,17 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: 'AIzaSyB_f9BeIed-SFjz6PetHpXbTZNRFBc3caQ',
-    authDomain: 'experience-shop.firebaseapp.com',
-    databaseURL: 'https://experience-shop.firebaseio.com',
-    projectId: 'experience-shop',
-    storageBucket: 'experience-shop.appspot.com',
-    messagingSenderId: '832283039558',
-    appId: '1:832283039558:web:ce0f7eff6059f8b9783747',
-    measurementId: 'G-LEHXWG3VY1'
+    apiKey: '',
+    authDomain: '',
+    databaseURL: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: '',
+    appId: '',
+    measurementId: ''
   },
   agm: {
-    apiKey: 'AIzaSyAWSZSCMxTRonYQe3L94wvCEd-gJCbrVkk',
+    apiKey: '',
     libraries: ['places']
   }
 };
